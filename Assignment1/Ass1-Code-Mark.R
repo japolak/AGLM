77c1caee1712be8abc403079fbd808e9ed823907
mcmahom5 <mcmahom5@tcd.ie>
2020-03-17 21:15:19 +0000

    task 4

--- Assignment1/Ass1-Code-Mark.R
+++ Assignment1/Ass1-Code-Mark.R
@@ -33,8 +33,33 @@ c3 <- c(2800, 2900, 2985, 3050)
 c4 <- c(2600, 2700, 2600, 2765)
 
 
-m <- matrix(c(c1, c2, c3, c4), nrow=4)
 
-boxplot(m, xlab="Mixing technique", ylab="Compressive Strength")
+boxplot(m[,2:5], xlab="Mixing technique", ylab="Compressive Strength")
 
+library(reshape)
+
+m <- data.frame(index=c(1,2,3,4), c1, c2, c3, c4)
+m2 <- melt(m, id.vars="index")
+
+aov(value~index, data=m2)
+
+?melt
+
+
+
+# task 4
+munich <- read.csv("munich.csv")
+munich$location <- factor(munich$location)
+
+
+pairs(munich)
+
+f <- lm(rent~area*location + yearc, data=munich)
+par(mfrow=c(2,2))
+plot(f1)
+summary(f1)
+
+f2 <- lm(rent~area, data=munich)
+
+anova(f, f2)
 
